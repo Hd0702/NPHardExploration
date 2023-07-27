7a0b19f0b2b8f5e59f1a14d15f65167905abbc74
Hd0702 <haydendonofrio@gmail.com>
2023-07-26 21:02:25 -0500

    Moved some stuff around

--- LAB02/Node.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Hayden Donofrio on 9/23/18.
-//
-
-#include "Node.h"
-bool operator < (const Node & lhs, const Node & rhs){
-    return (lhs.distance+ lhs.weight) > (rhs.distance + rhs.weight);
-}
